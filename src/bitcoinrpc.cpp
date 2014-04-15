7f0b006f27019943221264ff848884a1459058fb
whitecoiner <tattoocoin@outlook.com>
2014-04-15 22:50:21 +0800

    Update logo and change port

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -43,7 +43,7 @@ void ThreadRPCServer3(void* parg);
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 24071 : 15711;
+    return GetBoolArg("-testnet", false) ? 24071 : 15815;
 }
 
 Object JSONRPCError(int code, const string& message)
